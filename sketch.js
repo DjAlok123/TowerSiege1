ac06fe290820e62b022ce9c00b3366561a1ed73c
DjAlok123 <69834328+DjAlok123@users.noreply.github.com>
2020-12-29 13:41:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -123,8 +123,7 @@ function draw() {
 	box14.display();
 	box15.display();
 	box16.display();
-	
-	text("Drag the Hexagonal stone and release it,launch it towards the blocks.", 100, 100);
+
 
 //---------------------------------------------------------------------
        //Building 2
